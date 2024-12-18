65778ac10a274c32c7919fb278d12c1eec024646
bemnetberhanu <bemnetberhanu98@gmail.com>
2024-12-18 13:11:58 +0300

    first c

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+   
+<form action="index.php"method="post">
+  <label for="">user name</label>
+  <input type="text"name="country">
+  <label for="">password</label>
+  <input type="submit" value=" submit">
+</form>
+
+
+
+
+</body>
+</html>
+
+<?php
+
+
+
+
+
+
+?>
+ 
+
+  
